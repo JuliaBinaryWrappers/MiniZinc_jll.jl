b984056c248906f49e7494f9225865aa12a4c87d
jlbuild <juliabuildbot@gmail.com>
2022-05-03 04:27:34 +0000

    MiniZinc_jll build 2.6.2+0

---
+++ src/MiniZinc_jll.jl
@@ -0,0 +1,9 @@
+# Use baremodule to shave off a few KB from the serialized `.ji` file
+baremodule MiniZinc_jll
+using Base
+using Base: UUID
+import JLLWrappers
+
+JLLWrappers.@generate_main_file_header("MiniZinc")
+JLLWrappers.@generate_main_file("MiniZinc", UUID("3677d96b-3d39-5184-a844-8e8b2839af35"))
+end  # module MiniZinc_jll
