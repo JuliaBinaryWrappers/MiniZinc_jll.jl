05f30c76e6f199fa149477b933d5cec081241431
jlbuild <juliabuildbot@gmail.com>
2026-02-12 04:57:36 +0000

    MiniZinc_jll build 2.9.5+0

--- src/MiniZinc_jll.jl
+++ src/MiniZinc_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("MiniZinc")
-JLLWrappers.@generate_main_file("MiniZinc", UUID("3677d96b-3d39-5184-a844-8e8b2839af35"))
+JLLWrappers.@generate_main_file("MiniZinc", Base.UUID("3677d96b-3d39-5184-a844-8e8b2839af35"))
 end  # module MiniZinc_jll
